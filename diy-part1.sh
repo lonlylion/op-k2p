a76ded7dfd9dcc04efbb2db7b067af225c7476b1
lonlylion <57823665+lonlylion@users.noreply.github.com>
2022-01-27 13:39:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git mtk https://github.com/Nossiac/mtk-openwrt-feeds;lede-17.01' >>feeds.conf.default
