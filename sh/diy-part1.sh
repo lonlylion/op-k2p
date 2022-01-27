907bb53395281ed96c207104aa21e124c04d9bb6
lonlylion <57823665+lonlylion@users.noreply.github.com>
2022-01-27 19:57:42 +0800

    Update diy-part1.sh

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -17,3 +17,10 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git mtk https://github.com/Nossiac/mtk-openwrt-feeds;lede-17.01' >>feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+
+
+
